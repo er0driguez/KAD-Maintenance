3e7d68c77bc98e803b3d6e6752e6f1ace596afc5
Evelyn Rodriguez <evelynr812@gmail.com>
2022-10-14 00:02:23 -0400

    first commit - nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>
+            KAD
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={ click ? 'fa-solid fa-x' : 'fa-solid fa-bars' } />
+          </div>
+          <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                Contact Us
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {/* { button && <Button buttonStyle='btn--outline'> SIGN UP </Button>} */}
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
